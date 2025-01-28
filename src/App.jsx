aa0ba19382ada5f9f59637a7b205da6fb8d65e29
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2025-01-28 20:47:06 +0500

    complete adbout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import Navbar from './components/Navbar'
 import Hero from './Pages/Hero'
 import Statusbar from './Pages/Statusbar'
+import About from "../src/Pages/About/index"
 
 function App() {
 
   return (
     <>
-
       <div className="bg-gray-200 h-auto w-full">
-        
         <Navbar/>
         <Hero/>
         <Statusbar/>
+        <About />
       </div>
       
     </>
---
+++ src/Pages/About/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import AboutImg from "../../assets/AboutImg.png";
+
+const index = () => {
+  return (
+    <div className="flex justify-between flex-col md:flex-row px-5">
+      <div className="flex justify-start items-start flex-col ">
+        <div className="text-5xl text-black pt-7 flex gap-4">
+          <div className="flex justify-start items-center gap-5">
+            <h4>
+              <span className="text-xl sm:text-2xl md:text-4xl lg:text-5xl">*</span>
+              (design) FORLIO
+            </h4>
+            <div className="border-b border-8 border-gray-500 w-96 mt-3 hidden lg:block"></div>
+          </div>
+          <h1 className="text-5xl md:ml-4">2025</h1>
+        </div>
+        <div className="flex justify-center items-start gap-8 flex-col ">
+          <div className="text-xl sm:text-2xl md:text-4xl lg:text-5xl w-2/4 pt-28 ">
+            Hey there! I'm Uzitrake. A{" "}
+            <span className="text-pink-700">Fullstack Developer</span> Based in
+            Kenya(KE)
+          </div>
+          <div>
+            Current works : HYPERCRITICAL STUDIO <br />
+            Available for freelance
+            <br /> Software engineer
+          </div>
+        </div>
+      </div>
+      <div className="w-1/2">
+        <img src={AboutImg} alt="About Img" className="pt-10 w-full" />
+      </div>
+    </div>
+  );
+};
+
+export default index;
